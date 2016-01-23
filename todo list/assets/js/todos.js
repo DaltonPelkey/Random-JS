80cca3b434cb6f21da1abbfe05fd8bf19c04d3d9
Dalton Pelkey <popatop2260@gmail.com>
2016-01-23 12:55:23 -0600

    working commit

---
+++ todo list/assets/js/todos.js
@@ -0,0 +1,25 @@
+//Check off specific todo
+$("ul").on("click", "li", function(){
+  $(this).toggleClass("completed");
+});
+
+//click on x to delete todo
+$("ul").on("click", "span", function(e){
+  $(this).parent().fadeOut(500, function(){
+    $(this).remove();
+  });
+  e.stopPropagation();
+});
+
+$("input[type='text'").keypress(function(e){
+  if (e.which === 13) {
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
+  }
+});
+
+$(".fa-plus").click(function(){
+  $("input[type='text'").fadeToggle();
+  $(this).toggleClass('shown');
+})
